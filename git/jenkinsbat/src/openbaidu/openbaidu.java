b83511fa7874bd441a91b697a98c2a128debe973
233133st <1628932503@qq.com>
2019-04-28 10:06:04 +0800

    a.doc

---
+++ git/jenkinsbat/src/openbaidu/openbaidu.java
@@ -0,0 +1,19 @@
+package openbaidu;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class openbaidu {
+	public static WebDriver driver;
+	public static String url;
+	
+	@Test
+	public void open() {
+		System.setProperty("webdriver.chrome.driver","./tools/chromedriver.exe");
+		driver=new ChromeDriver();
+		//∂®“ÂÕ¯÷∑
+		url="https://www.baidu.com/";
+		driver.get(url);
+	}
+}
